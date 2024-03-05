5fac6894b0b9e69e86fc592752ea08573ea88aca
TDMwangi <teddymwangi65@gmail.com>
2024-03-06 01:12:54 +0300

    feat: add the App() function

--- src/App.jsx
+++ src/App.jsx
@@ -2,3 +2,10 @@ import QRCode from 'qrcode';
 import { useState } from 'react';
 
 import './App.css';
+
+function App() {
+  const [url, setUrl] = useState('');
+  const [qrcode, setQrcode] = useState('');
+
+  const generateQRCode = () => {}
+}
