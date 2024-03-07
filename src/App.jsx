62274b724b31d48ef6d1a908395e90edb4859cc0
TDMwangi <teddymwangi65@gmail.com>
2024-03-07 07:10:40 +0300

    feat: add the generateQRCode function

--- src/App.jsx
+++ src/App.jsx
@@ -7,5 +7,11 @@ function App() {
   const [url, setUrl] = useState('');
   const [qrcode, setQrcode] = useState('');
 
-  const generateQRCode = () => {}
+  const generateQRCode = () => {
+    QRCode.toDataURL(url, (err, url) => {
+      if (err) {
+        return console.error(err);
+      }
+    });
+  };
 }
