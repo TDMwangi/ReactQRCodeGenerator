91aecc4be24de90bbdfabc439a61923280a18663
TDMwangi <teddymwangi65@gmail.com>
2024-03-09 13:46:16 +0300

    add the downloadQRCode() function

--- src/App.jsx
+++ src/App.jsx
@@ -16,4 +16,11 @@ function App() {
       setQrcode(url);
     });
   };
+
+  const downloadQRCode = () => {
+    const link = document.createElement('a');
+    link.download = 'qrcode.png';
+    link.href = qrcode;
+    link.click();
+  };
 }
